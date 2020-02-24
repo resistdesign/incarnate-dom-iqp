9bc6c8c9ffc4e49a9f8f5bcb114e26f78945be60
Ryan Graff <ryantgraff@resistdesign.com>
2020-02-23 21:35:31 -0500

    feat: (App) Add SRACL layout and styling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,104 @@
+import './App/Assets/Fonts/Gasalt/stylesheet.css';
 import {hot} from 'react-hot-loader';
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import styled, {createGlobalStyle} from 'styled-components';
 import Incarnate, {LifePod} from 'incarnate-dom';
 import UUIDV4 from 'uuid/v4';
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardActions,
+  CardMedia,
+  CardActionArea,
+  CssBaseline,
+  Typography
+} from '@material-ui/core';
+import {
+  createMuiTheme,
+  ThemeProvider
+} from '@material-ui/core/styles';
+import SRACLMUITheme from '@resistdesign/sracl-mui-theme';
 import {ItemQueueProcessor} from './index';
+import Logo from './Assets/Graphics/zap-logo.svg';
 
+const THEME = createMuiTheme(SRACLMUITheme);
 const GlobalStyle = createGlobalStyle`
-html,
-body,
-#app-root {
+  html,
+  body,
+  #app-root {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    justify-content: flex-start;
+  }
+`;
+const Base = styled(Box)`
   margin: 0;
   padding: 0;
-  width: 100vw;
-  height: 100vh;
-  overflow: hidden;
+  box-sizing: border-box;
   display: flex;
   flex-direction: column;
   align-items: stretch;
   justify-content: flex-start;
-  font-family: sans-serif;
-}
 `;
-const Base = styled.div`
+const HeaderBox = styled(Box)`
+  box-sizing: border-box;
+  padding: 2em;
+`;
+const LogoImg = styled.img`
+  width: 7.5em;
+`;
+const Title = styled(Typography).attrs(p => ({
+  ...p,
+  style: {
+    ...p.style,
+    fontFamily: 'gasaltblack, sans-serif',
+    letterSpacing: '0.05em'
+  }
+}))`
   
 `;
-const Row = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: stretch;
-  justify-content: stretch;
+const SectionGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 4em;
+  padding: 2em;
+  box-sizing: border-box;
+  
+  @media (max-width: 620px) {
+    grid-template-columns: 1fr;
+  }
 `;
-const DisplayBox = styled.div`
-  flex: 1 0 auto;
-  margin: 1em;
-  border: solid 0.05em darkgray;
-  background-color: lightgray;
-  display: flex;
-  flex-direction: column;
+const SubSectionBox = styled.div`
+  padding: 1em;
+`;
+const RowBox = styled(SubSectionBox)`
+  display: grid;
+  grid-auto-flow: column;
   align-items: center;
   justify-content: flex-start;
-  max-height: 25em;
-  overflow: auto;
-  
-  & > h3 {
-    position: sticky;
-    top: 1em;
-    margin: 0.5em;
-    padding: 0.5em;
-    background-color: lightgray;
-    border-radius: 1em;
-  }
+  grid-gap: 1em;
 `;
-const DisplayButton = styled(DisplayBox)`
-  cursor: pointer;
-  user-select: none;
-  border-radius: 1em;
-  justify-content: center;
-  
-  & > h3 {
-    background-color: unset;
-  }
-  
-  &:hover {
-    background-color: #fefefe;
-  }
-  
-  &:active {
-    background-color: #333333;
-    color: #ffffff;
-  }
-`;
+const Section = ({title = '', children, ...props} = {}) => (
+  <Box
+    {...props}
+  >
+    {!!title ? (<Typography variant='h6'>{title}</Typography>) : undefined}
+    {children}
+  </Box>
+);
+const SubSection = ({title = '', children, ...props} = {}) => (
+  <SubSectionBox
+    {...props}
+  >
+    {!!title ? (<Typography>{title}</Typography>) : undefined}
+    {children}
+  </SubSectionBox>
+);
 
 const PROCESSING_COUNT_MAP = {};
 
@@ -78,195 +107,224 @@ export class App extends Component {
 
   render() {
     return (
-      <Base>
+      <Fragment>
         <GlobalStyle/>
-        <Incarnate
-          name='TransformPairs'
+        <ThemeProvider
+          theme={THEME}
         >
-          <LifePod
-            name='ParentItemID'
-            factory={() => UUIDV4()}
-          />
-          <LifePod
-            name='ItemProcessor'
-            dependencies={{
-              parentItemID: 'ParentItemID'
-            }}
-            strict
-            factory={({parentItemID}) => async ({id, ...item} = {}) => await new Promise((res, rej) => setTimeout(
-              () => {
-                PROCESSING_COUNT_MAP[id] = typeof PROCESSING_COUNT_MAP[id] === 'number' ?
-                  PROCESSING_COUNT_MAP[id] + 1 :
-                  1;
-
-                return Math.random() < 0.25 ?
-                  rej(new Error('Unknown Error')) :
-                  res({
-                    id: `SAVED_${id}`,
-                    processedXTimes: PROCESSING_COUNT_MAP[id],
-                    parentItemID,
-                    ...item
-                  });
-              },
-              2500
-            ))}
-          />
-          <LifePod
-            name='RunningCount'
-            factory={() => 0}
-          />
-          <LifePod
-            name='Map'
-            factory={() => ({})}
-          />
-          <LifePod
-            name='ErrorMap'
-            factory={() => ({})}
-          />
-          <LifePod
-            name='Existing'
-            factory={() => ({})}
-          />
-          <ItemQueueProcessor
-            name='MapQueueProcessor'
-            shared={{
-              InputMap: 'Map',
-              OutputMap: 'Existing',
-              ItemProcessor: 'ItemProcessor',
-              ErrorMap: 'ErrorMap'
-            }}
-            batchSize={3}
-            batchDelayMS={1500}
-          />
-          <Row>
-            <LifePod
-              getters={{
-                getRunningCount: 'RunningCount'
-              }}
-              setters={{
-                setRunningCount: 'RunningCount',
-                setMap: 'Map'
-              }}
-              mapToProps={({
-                             getRunningCount,
-                             setRunningCount,
-                             setMap
-                           }) => ({
-                onClick: () => {
-                  const id = UUIDV4();
-                  const item = {
-                    id,
-                    properties: {},
-                    content: `I'm an item!`
-                  };
-
-                  setRunningCount(getRunningCount() + 1);
-
-                  setMap(item, id);
-
-                  if (Math.random() < 0.4) {
-                    // Make it stale. (Mmmmmmmm, it's like late night fries!)
-                    setTimeout(() => setMap({...item, note: 'Updated mid-processing'}, id), Math.random() * 2000);
-                  }
-                }
-              })}
+          <CssBaseline/>
+          <Base
+            display='flex'
+            flexDirection='column'
+            alignItems='stretch'
+            justifyContent='flex-start'
+          >
+            <HeaderBox
+              display='flex'
+              flexDirection='row'
+              alignItems='center'
+              justifyContent='flex-start'
             >
-              <DisplayButton>
-                <h3>Add Item</h3>
-              </DisplayButton>
-            </LifePod>
-          </Row>
-          <Row>
-            <DisplayBox>
-              <h3>Map</h3>
-              <LifePod
-                dependencies={{
-                  map: 'Map'
-                }}
+              <LogoImg
+                src={Logo}
+              />
+              <Title
+                variant='h5'
               >
-                {({map}) => (
-                  <pre>{JSON.stringify(map, null, '  ')}</pre>
-                )}
-              </LifePod>
-            </DisplayBox>
-            <DisplayBox>
-              <h3>Processing Queue</h3>
-              <LifePod
-                dependencies={{
-                  runningCount: 'RunningCount',
-                  processingRequests: 'MapQueueProcessor.Processing',
-                  existing: 'Existing'
-                }}
+                Incarnate DOM <Title display='inline' variant='h5' color='textSecondary'>Item Queue Processor</Title>
+              </Title>
+            </HeaderBox>
+            <SectionGrid>
+              <Incarnate
+                name='TransformPairs'
               >
-                {({runningCount, processingRequests, existing = {}}) => (
-                  <span>
+                <LifePod
+                  name='ParentItemID'
+                  factory={() => UUIDV4()}
+                />
+                <LifePod
+                  name='ItemProcessor'
+                  dependencies={{
+                    parentItemID: 'ParentItemID'
+                  }}
+                  strict
+                  factory={({parentItemID}) => async ({id, ...item} = {}) => await new Promise((res, rej) => setTimeout(
+                    () => {
+                      PROCESSING_COUNT_MAP[id] = typeof PROCESSING_COUNT_MAP[id] === 'number' ?
+                        PROCESSING_COUNT_MAP[id] + 1 :
+                        1;
+
+                      return Math.random() < 0.25 ?
+                        rej(new Error('Unknown Error')) :
+                        res({
+                          id: `SAVED_${id}`,
+                          processedXTimes: PROCESSING_COUNT_MAP[id],
+                          parentItemID,
+                          ...item
+                        });
+                    },
+                    2500
+                  ))}
+                />
+                <LifePod
+                  name='RunningCount'
+                  factory={() => 0}
+                />
+                <LifePod
+                  name='Map'
+                  factory={() => ({})}
+                />
+                <LifePod
+                  name='ErrorMap'
+                  factory={() => ({})}
+                />
+                <LifePod
+                  name='Existing'
+                  factory={() => ({})}
+                />
+                <ItemQueueProcessor
+                  name='MapQueueProcessor'
+                  shared={{
+                    InputMap: 'Map',
+                    OutputMap: 'Existing',
+                    ItemProcessor: 'ItemProcessor',
+                    ErrorMap: 'ErrorMap'
+                  }}
+                  batchSize={3}
+                  batchDelayMS={1500}
+                />
+                <Row>
+                  <LifePod
+                    getters={{
+                      getRunningCount: 'RunningCount'
+                    }}
+                    setters={{
+                      setRunningCount: 'RunningCount',
+                      setMap: 'Map'
+                    }}
+                    mapToProps={({
+                                   getRunningCount,
+                                   setRunningCount,
+                                   setMap
+                                 }) => ({
+                      onClick: () => {
+                        const id = UUIDV4();
+                        const item = {
+                          id,
+                          properties: {},
+                          content: `I'm an item!`
+                        };
+
+                        setRunningCount(getRunningCount() + 1);
+
+                        setMap(item, id);
+
+                        if (Math.random() < 0.4) {
+                          // Make it stale. (Mmmmmmmm, it's like late night fries!)
+                          setTimeout(() => setMap({...item, note: 'Updated mid-processing'}, id), Math.random() * 2000);
+                        }
+                      }
+                    })}
+                  >
+                    <DisplayButton>
+                      <h3>Add Item</h3>
+                    </DisplayButton>
+                  </LifePod>
+                </Row>
+                <Row>
+                  <DisplayBox>
+                    <h3>Map</h3>
+                    <LifePod
+                      dependencies={{
+                        map: 'Map'
+                      }}
+                    >
+                      {({map}) => (
+                        <pre>{JSON.stringify(map, null, '  ')}</pre>
+                      )}
+                    </LifePod>
+                  </DisplayBox>
+                  <DisplayBox>
+                    <h3>Processing Queue</h3>
+                    <LifePod
+                      dependencies={{
+                        runningCount: 'RunningCount',
+                        processingRequests: 'MapQueueProcessor.Processing',
+                        existing: 'Existing'
+                      }}
+                    >
+                      {({runningCount, processingRequests, existing = {}}) => (
+                        <span>
                     {!!processingRequests ? 'Yes' : 'No'}<br/>
                     Created: {Object.keys(existing).length} out of {runningCount}
                   </span>
-                )}
-              </LifePod>
-            </DisplayBox>
-            <DisplayBox>
-              <h3>Existing</h3>
-              <LifePod
-                dependencies={{
-                  existing: 'Existing'
-                }}
-              >
-                {({existing}) => (
-                  <pre>{JSON.stringify(existing, null, '  ')}</pre>
-                )}
-              </LifePod>
-            </DisplayBox>
-          </Row>
-          <Row>
-            <DisplayBox>
-              <LifePod
-                dependencies={{
-                  errorMap: 'ErrorMap',
-                  mapQueueProcessorController: 'MapQueueProcessor.Controller'
-                }}
-                mapToProps={p => p}
-              >
-                {({errorMap = {}, mapQueueProcessorController}) => {
-                  const errorKeyList = Object.keys(errorMap);
+                      )}
+                    </LifePod>
+                  </DisplayBox>
+                  <DisplayBox>
+                    <h3>Existing</h3>
+                    <LifePod
+                      dependencies={{
+                        existing: 'Existing'
+                      }}
+                    >
+                      {({existing}) => (
+                        <pre>{JSON.stringify(existing, null, '  ')}</pre>
+                      )}
+                    </LifePod>
+                  </DisplayBox>
+                </Row>
+                <Row>
+                  <DisplayBox>
+                    <LifePod
+                      dependencies={{
+                        errorMap: 'ErrorMap',
+                        mapQueueProcessorController: 'MapQueueProcessor.Controller'
+                      }}
+                      mapToProps={p => p}
+                    >
+                      {({errorMap = {}, mapQueueProcessorController}) => {
+                        const errorKeyList = Object.keys(errorMap);
 
-                  return (
-                    <div>
-                      <h5>{errorKeyList.length} Errors</h5>
-                      <ul>
-                        {errorKeyList.map(k => (
-                          <li key={`Message:${k}`}>
-                            {k}:&nbsp;{errorMap[k] && errorMap[k].message}
-                            &nbsp;
-                            <button
-                              onClick={() => mapQueueProcessorController.dismissError(k)}
-                            >
-                              Dismiss
-                            </button>
-                            &nbsp;
-                            <button
-                              onClick={() => mapQueueProcessorController.retryError(k)}
-                            >
-                              Retry
-                            </button>
-                            &nbsp;
-                            <button
-                              onClick={() => mapQueueProcessorController.dismissError(k, true)}
-                            >
-                              Cancel
-                            </button>
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                  );
-                }}
-              </LifePod>
-            </DisplayBox>
-          </Row>
-        </Incarnate>
-      </Base>
+                        return (
+                          <div>
+                            <h5>{errorKeyList.length} Errors</h5>
+                            <ul>
+                              {errorKeyList.map(k => (
+                                <li key={`Message:${k}`}>
+                                  {k}:&nbsp;{errorMap[k] && errorMap[k].message}
+                                  &nbsp;
+                                  <button
+                                    onClick={() => mapQueueProcessorController.dismissError(k)}
+                                  >
+                                    Dismiss
+                                  </button>
+                                  &nbsp;
+                                  <button
+                                    onClick={() => mapQueueProcessorController.retryError(k)}
+                                  >
+                                    Retry
+                                  </button>
+                                  &nbsp;
+                                  <button
+                                    onClick={() => mapQueueProcessorController.dismissError(k, true)}
+                                  >
+                                    Cancel
+                                  </button>
+                                </li>
+                              ))}
+                            </ul>
+                          </div>
+                        );
+                      }}
+                    </LifePod>
+                  </DisplayBox>
+                </Row>
+              </Incarnate>
+            </SectionGrid>
+          </Base>
+        </ThemeProvider>
+      </Fragment>
     );
   }
 }
